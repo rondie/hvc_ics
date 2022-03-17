a00b0053fbd9811182935eb31e8ea6c4da93e833
root <root@rocinante.ron>
2022-03-17 20:14:11 +0100

    added a simple frontpage

--- app/ics.py
+++ app/ics.py
@@ -3,9 +3,20 @@ from io import BytesIO
 from app.get_data import get_hvc_data
 from app.make_ics import make_ics
 
-from flask import Flask, send_file
+from flask import Flask, render_template, request, send_file
 
 app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def data():
+    if request.method == "GET":
+        return render_template("data.html")
+    if request.method == "POST":
+        postalcode = request.form['Postal Code']
+        streetnumber = request.form['Streetnumber']
+        ophaaldata = get_hvc_data(postalcode, streetnumber)
+        return render_template("data.html", **locals())
 
 
 @app.route('/hvc-<postalcode>-<streetnumber>.ics')
